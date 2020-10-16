224a7f6202c6d3db0aa432c713bf04f0934a2d1b
Vlad-Zhuravsky <61624540+Vlad-Zhuravsky@users.noreply.github.com>
2020-10-16 17:59:07 +0300

    Update cats.php
    Rewrited

--- Cats/cats.php
+++ Cats/cats.php
@@ -9,15 +9,25 @@ class Cat {
     }
 
     function fight($anotherCat) {
-     if ($this->age > $anotherCat->age || $this->weight > $anotherCat->weight || $this->strength > $anotherCat->strength){
+     if ($this->age < $anotherCat->age || $this->weight > $anotherCat->weight || $this->strength > $anotherCat->strength){
        true;
-     }else false; 
-      
+       echo "win";
+     }else{
+       false; 
+       echo "lose";
+     } 
     }
 }
 
-$cat1 = new Cat(4,4,4);
-$cat2 = new Cat(3,3,3);
+$cat1 = new Cat();
+$cat1->age = 10;
+$cat1->weight=2;
+$cat1->strength=12;
+
+$cat2 = new Cat();
+$cat2->age = 3;
+$cat2->weight=5;
+$cat2->strength=10;
 
 $cat1->fight($cat2);
 
